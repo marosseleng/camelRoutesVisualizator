1f9a8e9d22f8f33b403a8d255ea87d08a7866b1e
maroselo <422624@mail.muni.cz>
2015-05-12 11:36:11 +0200

    Add enum for board type and resources for Raspberry Pi B+.

---
+++ src/main/java/cz/muni/fi/pb138/visualizator/BoardType.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pb138.visualizator;
+
+/**
+ * Enum to handle board choosing
+ * @author <a href="mailto:xseleng@fi.muni.cz">Maros Seleng</a>
+ */
+public enum BoardType {
+    BEAGLEBONE,
+    RASPBERRY_PI,
+    CUBIEBOARD,
+    COMMON
+}
